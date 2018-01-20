9f281fad5078ec4c57a907be2a707afd54c1e84c
Braulio Cassule <brauliocassule94@gmail.com>
2018-01-20 15:01:18 +0100

    food prices

--- lib/model/menu.dart
+++ lib/model/menu.dart
@@ -13,73 +13,73 @@ class Menu{
         id: "2",
         image: "assets/images/menu3.png",
         name: "Salad",
-        price: "\$12"
+        price: "\$8"
     ),
     const Food(
         id: "3",
         image: "assets/images/menu11.png",
         name: "Mashroom Sauce",
-        price: "\$12"
+        price: "\$4"
     ),
     const Food(
         id: "4",
         image: "assets/images/menu13.png",
         name: "Seefood Soup",
-        price: "\$12"
+        price: "\$30"
     ),
     const Food(
         id: "5",
         image: "assets/images/menu4.png",
         name: "",
-        price: "\$12"
+        price: "\$00"
     ),
     const Food(
         id: "6",
         image: "assets/images/menu12.png",
         name: "Fillet & Prawns",
-        price: "\$12"
+        price: "\$22"
     ),
     const Food(
         id: "7",
         image: "assets/images/menu10.png",
         name: "Meat Roll",
-        price: "\$12"
+        price: "\$19"
     ),
     const Food(
         id: "8",
         image: "assets/images/menu7.png",
         name: "Chicken Salad",
-        price: "\$12"
+        price: "\$25"
     ),
     const Food(
         id: "9",
         image: "assets/images/menu5.png",
         name: "Bolonesa Pasta",
-        price: "\$12"
+        price: "\$7"
     ),
     const Food(
         id: "10",
         image: "assets/images/menu9.png",
         name: "Overcooked Meat",
-        price: "\$12"
+        price: "\$63"
     ),
     const Food(
         id: "11",
         image: "assets/images/menu2.png",
         name: "",
-        price: "\$12"
+        price: "\$18"
     ),
     const Food(
         id: "12",
         image: "assets/images/menu8.png",
         name: "Chicken Fillet Salad",
-        price: "\$12"
+        price: "\$43"
     ),
     const Food(
         id: "13",
         image: "assets/images/menu14.png",
         name: "",
-        price: "\$12"
+        price: "\$3"
     ),
   ];
 }
