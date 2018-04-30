a4e540ba765a1f7ca60e8153705073c6376b47d3
Braulio Cassule <brauliocassule94@gmail.com>
2018-04-30 16:39:02 +0100

    add copyWith constructor

--- lib/model/food.dart
+++ lib/model/food.dart
@@ -5,11 +5,23 @@ class Food {
   final String name;
   final String image;
   final String id;
+  final int quantity;
 
   const Food({
     this.id,
     this.image,
     this.name,
-    this.price
-  });
+    this.price,
+    this.quantity: 0,
+  }): assert(quantity != null && quantity >= 0);
+
+  Food copyWith({String id, String image, String name, String price, int quantity}){
+    return new Food(
+      id: id ?? this.id,
+      image: image ?? this.image,
+      name: name ?? this.name,
+      price: price ?? this.price,
+      quantity: quantity ?? this.quantity
+    );
+  }
 }
